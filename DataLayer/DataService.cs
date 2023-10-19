9154a5698276afce1659d3ba7c476ebf96d837c3
xframee <54414538+xframee@users.noreply.github.com>
2023-10-19 19:19:00 +0200

    added update functionality

--- DataLayer/DataService.cs
+++ DataLayer/DataService.cs
@@ -15,4 +15,51 @@ public class DataService
         var db = new NorthwindContext();
         return db.Categories.FirstOrDefault(x => x.Id == categoryId);
     }
+    public bool DeleteCategory(Category category)
+    {
+        return DeleteCategory(category.Id);
+    }
+    public bool DeleteCategory(int categoryId)
+    {
+        var db = new NorthwindContext();
+        var category = db.Categories.FirstOrDefault(x => x.Id == categoryId);
+        if (category != null)
+        {
+            db.Categories.Remove(category);
+            //db.Remove(category);
+            return db.SaveChanges() > 0;
+        }
+        return false;
+    }
+
+    public Category CreateCategory(string name, string description)
+    {
+        var db = new NorthwindContext();
+        var id = db.Categories.Max(x => x.Id) + 1;
+        var category = new Category
+        {
+            Id = id,
+            Name = name,
+            Description = description
+        };
+        db.Add(category);
+        db.SaveChanges();
+        return category;
+    }
+
+    public bool UpdateCategory(int Id, string name, string description)
+    {
+        var db = new NorthwindContext();
+
+        DeleteCategory(Id);
+
+        var category = new Category
+        {
+            Id = Id,
+            Name = name,
+            Description = description
+        };
+        db.Add(category);
+        return db.SaveChanges() > 0;
+    }
 }
