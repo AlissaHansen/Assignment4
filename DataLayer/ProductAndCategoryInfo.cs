c7143350c461475664febfd633d9813fdc98781a
xframee <alexander@kmax.dk>
2023-10-20 14:22:17 +0200

    created new return class

---
+++ DataLayer/ProductAndCategoryInfo.cs
@@ -0,0 +1,11 @@
+namespace DataLayer;
+
+public class ProductAndCategoryInfo
+{
+    public int ProductId { get; set; }
+    public string ProductName { get; set; }
+    public int UnitPrice { get; set; }
+    public string QuantityPerUnit { get; set; }
+    public int UnitsInStock { get; set; }
+    public string CategoryName { get; set; }
+}
